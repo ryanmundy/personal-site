2f5af388250c1ac54a18d9b5355f16941113eed8
Ryan Mundy <ryanmundy34@gmail.com>
2019-03-08 13:17:25 -0600

    adds navbar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,45 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 
 const Nav = (props) => (
   <div className="nav">
-    <Link to="/home">
+    {/* <Link to="/home">
       <img className="nav-image" src="logo3.png" alt="logo" />
-    </Link>
+    </Link> */}
+    <h1>Ryan Mundy</h1>
+    <h3>Full Stack Software Developer</h3>
+    <hr></hr>
+    <br/>
     <div className="nav-right">
       <Link className="nav-link" to="/home">
-        {/* Show this link if they are logged in or not,
-        but call this link 'Home' if they are logged in,
-        and call this link 'Login / Register' if they are not */}
-        {props.user.id ? 'Home' : 'Login / Register'}
+        Home
       </Link>
-      {/* Show the link to the info page and the logout button if the user is logged in */}
-      {props.user.id && (
-        <>
-          <Link className="nav-link" to="/info">
-            Info Page
-          </Link>
-          <LogOutButton className="nav-link"/>
-        </>
-      )}
-      {/* Always show this link since the about page is not protected */}
-      <Link className="nav-link" to="/about">
-        About
+      <Link className="nav-link" to="/resume">
+        Resume
+      </Link>
+      <Link className="nav-link" to="/skills">
+        Skills
+      </Link>
+      <Link className="nav-link" to="/contact">
+        Contact
       </Link>
     </div>
   </div>
 );
 
-// Instead of taking everything from state, we just want the user
-// object to determine if they are logged in
-// if they are logged in, we show them a few more links 
-// if you wanted you could write this code like this:
-// const mapStateToProps = ({ user }) => ({ user });
-const mapStateToProps = state => ({
-  user: state.user,
-});
-
-export default connect(mapStateToProps)(Nav);
+export default Nav;
