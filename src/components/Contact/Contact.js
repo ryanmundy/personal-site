ffccbe728bf75d6888042e3faff319f752c47b26
Ryan Mundy <ryanmundy34@gmail.com>
2019-03-14 14:44:42 -0500

    adds check for contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -3,7 +3,15 @@ import TextField from '@material-ui/core/TextField';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import { Card, Button } from '@material-ui/core';
 import axios from 'axios';
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { createMuiTheme } from "@material-ui/core/styles"
 
+const theme = createMuiTheme({
+    palette: {
+        primary: { main: "#000000" },
+        secondary: { main: "#000000" }
+    },
+});
 
 class Contact extends Component {
 
@@ -15,6 +23,9 @@ class Contact extends Component {
 
     submit = () => {
         console.log('in submit', this.state);
+        if(this.state.name===''||this.state.email===''||this.state.message===''){
+            alert('Please complete all fields!');
+        }else{
         axios({
             method: 'POST',
             url: '/contact',
@@ -24,6 +35,7 @@ class Contact extends Component {
         }).catch((error) => {
             console.log(error)
         });
+    }
     }
 
     //change handler for inputs
@@ -41,33 +53,38 @@ class Contact extends Component {
             <div className="div__container container__background--large">
                 <div>
                     <h1>Contact Me</h1>
-                    <hr></hr>
+                    {/* <hr></hr> */}
                 </div>
-                <div className="links">
+                {/* <div className="links">
                     <h3>If you like the work I do, connect with me on <a style={{ color: "white" }} target="_blank" href="https://www.linkedin.com/in/ryan-mundy-4b147b88/">LinkedIn!</a></h3>
                     <a target="_blank" href="https://www.linkedin.com/in/ryan-mundy-4b147b88/"><img className="linkImage" height="25px" src="Logo-White-48px-TM.png" alt="linkedin" /></a>
-                </div>
+                </div> */}
 
                 <div>
+                    <MuiThemeProvider theme={theme}>
                     <Card>
                         <TextField
                             placeholder="name"
                             type="text"
-                            onChange={this.handleChangeFor('name')} />
+                            onChange={this.handleChangeFor('name')}
+                            margin="normal" />
                         <TextField
                             placeholder="email"
                             type="text"
-                            onChange={this.handleChangeFor('email')} />
+                            onChange={this.handleChangeFor('email')}
+                            margin="normal" />
                             <br/>
                         <TextField
                             placeholder="message"
                             variant="outlined"
                             multiline rows="4"
                             type="text"
-                            onChange={this.handleChangeFor('message')} />
+                            onChange={this.handleChangeFor('message')}
+                            margin="normal" />
                             <br/>
-                            <Button onClick={this.submit} variant="outlined">Send</Button>
+                            <Button margin="normal" onClick={this.submit} variant="outlined">Send</Button>
                     </Card>
+                    </MuiThemeProvider>
                 </div>
             </div>
         )
