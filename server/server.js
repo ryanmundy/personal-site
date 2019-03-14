81a15fc995e5f4f30b8a57f48a864b3b7d366d3d
Ryan Mundy <ryanmundy34@gmail.com>
2019-03-14 13:54:30 -0500

    adds form

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 require('dotenv').config();
+const nodemailer = require('nodemailer');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -14,6 +15,35 @@ app.use(express.static('build'));
 
 // App Set //
 const PORT = process.env.PORT || 5000;
+
+// Routes //
+// POST route from contact form
+app.post('/contact', function (req, res) {
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: GMAIL_USER,
+      pass: GMAIL_PASS
+    }
+  });
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: GMAIL_USER,
+    subject: 'New message from contact form at tylerkrys.ca',
+    text: `${req.body.name} (${req.body.email}) says: ${req.body.message}`
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.render('contact-failure');
+    }
+    else {
+      res.render('contact-success');
+    }
+  });
+});
 
 /** Listen * */
 app.listen(PORT, () => {
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import { Card, Button } from '@material-ui/core';
+import Axios from 'axios';
 
-const Contact = () => (
-    <div>
-        <div className="div__container container__background--large">
-            <h1>Contact Me</h1>
-            <hr></hr>
-            <div className="links">
-                <h3>If you like the work I do, connect with me on <a style={{color: "white"}}target="_blank" href="https://www.linkedin.com/in/ryan-mundy-4b147b88/">LinkedIn!</a></h3>
-                <a target="_blank" href="https://www.linkedin.com/in/ryan-mundy-4b147b88/"><img className="linkImage" height="25px" src="Logo-White-48px-TM.png" alt="linkedin" /></a>
+class Contact extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        message: ''
+    }
+
+    submit = () => {
+        console.log('in submit', this.state);
+        
+    }
+
+    //change handler for inputs
+    handleChangeFor = (propertyName) => (event) => {
+        event.preventDefault();
+        this.setState({
+                ...this.state,
+                [propertyName]: event.target.value
+        });
+    }
+
+    render() {
+        return (
+
+            <div className="div__container container__background--large">
+                <div>
+                    <h1>Contact Me</h1>
+                    <hr></hr>
+                </div>
+                <div className="links">
+                    <h3>If you like the work I do, connect with me on <a style={{ color: "white" }} target="_blank" href="https://www.linkedin.com/in/ryan-mundy-4b147b88/">LinkedIn!</a></h3>
+                    <a target="_blank" href="https://www.linkedin.com/in/ryan-mundy-4b147b88/"><img className="linkImage" height="25px" src="Logo-White-48px-TM.png" alt="linkedin" /></a>
+                </div>
+
+                <div>
+                    <Card>
+                        <TextField
+                            placeholder="name"
+                            type="text"
+                            onChange={this.handleChangeFor('name')} />
+                        <TextField
+                            placeholder="email"
+                            type="text"
+                            onChange={this.handleChangeFor('email')} />
+                            <br/>
+                        <TextField
+                            placeholder="message"
+                            variant="outlined"
+                            multiline rows="4"
+                            type="text"
+                            onChange={this.handleChangeFor('message')} />
+                            <br/>
+                            <Button onClick={this.submit} variant="outlined">Send</Button>
+                    </Card>
+                </div>
             </div>
-        </div>
-    </div>
-);
+        )
+    }
+}
 
 export default Contact;
